a342dd4761f1b6cccff68f391a627e2bf305ea27
khyeji98 <gpwl98wkd@gmail.com>
2025-09-23 03:39:03 +0900

    create project

---
+++ RunDiary/RunDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunDiaryApp.swift
+//  RunDiary
+//
+//  Created by 김혜지 on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct RunDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
