35575895e92861dbb2e84044d6060ab3979fde82
khyeji98 <gpwl98wkd@gmail.com>
2025-10-29 14:39:13 +0900

    refactor: TimeInterval 포맷에 공백 추가 (#8)

--- Dependencies/CommonFoundation/Sources/CommonFoundation/TimeInterval+Extensions.swift
+++ Dependencies/CommonFoundation/Sources/CommonFoundation/TimeInterval+Extensions.swift
@@ -15,9 +15,9 @@ public extension TimeInterval {
         seconds %= 60
 
         if hours > 0 {
-            return String(format: "%d시간%02d분%02d초", hours, minutes, seconds)
+            return String(format: "%d시간 %0d분 %0d초", hours, minutes, seconds)
         } else {
-            return String(format: "%d분%02d초", minutes, seconds)
+            return String(format: "%d분 %0d초", minutes, seconds)
         }
     }
 }
