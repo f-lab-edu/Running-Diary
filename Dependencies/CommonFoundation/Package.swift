97c3e46b655cc8060c853e1b665bea447dc2fcb8
khyeji98 <gpwl98wkd@gmail.com>
2025-10-30 21:17:28 +0900

    refactor: CommonFoundation Package Manifest 수정 (#15)
    - platform 속성 설정

--- Dependencies/CommonFoundation/Package.swift
+++ Dependencies/CommonFoundation/Package.swift
@@ -5,16 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "CommonFoundation",
+    platforms: [.iOS(.v18)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CommonFoundation",
             targets: ["CommonFoundation"]
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "CommonFoundation"
         ),
